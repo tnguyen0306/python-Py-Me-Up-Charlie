717a61342bafec2c383cc3f64115571532ae9618
tnguyen0306 <tnguyen0306@gmail.com>
2021-02-01 17:04:48 -0600

    Beta 1.1.0

--- main.py
+++ main.py
@@ -3,5 +3,15 @@
 import os
 import csv
 
-budget_csv = os.path.join("..", "Resources", "budget_data.csv")
+budget_csv = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Resources", "budget_data.csv")
 
+with open(budget_csv) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ",")
+
+    csv_header = next(csv_file)
+    
+    month_count = int(len(list(csv_reader)))
+    print(month_count)
+#    for row in csv_reader:
+#        #month_count = len(row[0])
+ #       print(row)
